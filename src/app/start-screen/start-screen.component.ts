9d9f721ddbcb71f6ab4b894f97c833a0913a8eb7
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2023-03-16 19:50:39 +0100

    start screen added

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
   templateUrl: './start-screen.component.html',
   styleUrls: ['./start-screen.component.scss'],
 })
-export class StartScreenComponent {}
+export class StartScreenComponent {
+  constructor(private router: Router) {}
+
+  newGame() {
+    this.router.navigateByUrl('/game');
+  }
+}
