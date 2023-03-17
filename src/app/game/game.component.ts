148989f51a7f1d5f4cdf7dcd48e5b5a6f101b196
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2023-03-17 21:49:48 +0100

    all cards added and flip animation designed

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss'],
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string = '';
+  stackLength: number = 0;
+  game: Game = new Game();
+
+  ngOnInit(): void {
+    this.newGame();
+  }
 
   takeCard() {
+    this.stackLength = this.game.stack.length - 1;
+    this.currentCard = this.game.stack[this.stackLength];
+    this.game.stack.splice(this.stackLength, 1);
+    this.game.playedCards.push(this.currentCard);
     this.pickCardAnimation = true;
+  }
+
+  newGame() {
+    console.log(this.game);
   }
 }
---
+++ src/models/game.ts
@@ -0,0 +1,31 @@
+export class Game {
+  public players: string[] = [];
+  public stack: string[] = [];
+  public playedCards: string[] = [];
+  public currentPlayer: number = 0;
+
+  constructor() {
+    for (let i = 1; i < 14; i++) {
+      this.stack.push('s' + i);
+      this.stack.push('h' + i);
+      this.stack.push('c' + i);
+      this.stack.push('d' + i);
+    }
+
+    randomize(this.stack);
+  }
+}
+
+function randomize(array: string[]) {
+  let index = array.length,
+    randomIndex;
+
+  while (index != 0) {
+    randomIndex = Math.floor(Math.random() * index);
+    index--;
+
+    [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
+  }
+
+  return array;
+}
