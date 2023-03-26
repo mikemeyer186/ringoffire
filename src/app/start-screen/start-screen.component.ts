afac2376ed2955700abeea88f371ac40d385cb36
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2023-03-26 16:29:47 +0200

    changed startscreen to service

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,48 +1,16 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
-import { Game } from 'src/models/game';
-import { inject } from '@angular/core';
-import {
-  Firestore,
-  collectionData,
-  collection,
-  addDoc,
-} from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
 import { LoadDialogComponent } from '../load-dialog/load-dialog.component';
-
+import { StoreDataService } from '../store-data.service';
 @Component({
   selector: 'app-start-screen',
   templateUrl: './start-screen.component.html',
   styleUrls: ['./start-screen.component.scss'],
 })
 export class StartScreenComponent {
-  games$: Observable<any>;
-  firestore: Firestore = inject(Firestore);
-  gameObject: Game = new Game();
-
-  constructor(private router: Router, public dialog: MatDialog) {
-    const gameCollection = collection(this.firestore, 'games');
-    this.games$ = collectionData(gameCollection, { idField: 'id' });
-  }
-
-  newGame() {
-    this.addGame();
-  }
-
-  async addGame() {
-    const gameCollection = await collection(this.firestore, 'games');
-    await addDoc(gameCollection, this.gameObject.toJson()).then((game) => {
-      this.loadGame(game.id);
-    });
-  }
+  constructor(public sds: StoreDataService, public dialog: MatDialog) {}
 
   openLoadDialog(): void {
     this.dialog.open(LoadDialogComponent);
-  }
-
-  loadGame(id: string) {
-    this.router.navigateByUrl(`/game/${id}`);
   }
 }
--- src/app/store-data.service.ts
+++ src/app/store-data.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@angular/core';
 import { inject } from '@angular/core';
-import { Firestore, doc, deleteDoc } from '@angular/fire/firestore';
+import { Game } from 'src/models/game';
+import {
+  Firestore,
+  doc,
+  deleteDoc,
+  collection,
+  addDoc,
+  collectionData,
+} from '@angular/fire/firestore';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StoreDataService {
   oldGameID: string = '';
+  games$: Observable<any>;
   firestore: Firestore = inject(Firestore);
+  gameObject: Game = new Game();
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute) {
+    const gameCollection = collection(this.firestore, 'games');
+    this.games$ = collectionData(gameCollection, { idField: 'id' });
+  }
+
+  async newGame() {
+    const gameCollection = collection(this.firestore, 'games');
+    await addDoc(gameCollection, this.gameObject.toJson()).then((game) => {
+      this.loadGame(game.id);
+    });
+  }
+
+  loadGame(id: string) {
+    this.router.navigateByUrl(`/game/${id}`);
+  }
 
   async deleteGame() {
     this.getOldIdFromRoute();
