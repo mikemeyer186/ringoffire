63fc341358a4bfc6d3bd6ebe33923bf1e80608f1
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2023-04-01 01:01:26 +0200

    bugfixing

--- src/models/game.ts
+++ src/models/game.ts
@@ -4,7 +4,7 @@ export class Game {
   public playedCards: string[] = [];
   public currentPlayer: number = 1;
   public gameDate: Date = new Date();
-  public pickCardAnimation: any = false;
+  public pickCardAnimation: Boolean = false;
   public currentCard: string | undefined = '';
 
   constructor() {
